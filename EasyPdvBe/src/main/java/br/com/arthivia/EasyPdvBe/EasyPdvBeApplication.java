740c15177f2a4cfc7464dd215954746875404b89
joao-mourato <joaomouratocn@gmail.com>
2026-03-09 10:31:58 -0300

    First Commit

---
+++ EasyPdvBe/src/main/java/br/com/arthivia/EasyPdvBe/EasyPdvBeApplication.java
@@ -0,0 +1,13 @@
+package br.com.arthivia.EasyPdvBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPdvBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyPdvBeApplication.class, args);
+	}
+
+}
---
+++ EasyPdvBe/src/test/java/br/com/arthivia/EasyPdvBe/EasyPdvBeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arthivia.EasyPdvBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPdvBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
